8aa685179ee0e01dc708ef0c59b702519a008978
ofri <ofrigalperin@gmail.com>
2016-02-21 01:20:48 +0200

    more efficient code and nicer printing

--- functions/tools/read_files_contents_and_scores.m
+++ functions/tools/read_files_contents_and_scores.m
@@ -1,28 +1,37 @@
 function [ review_array, review_score ] = read_files_contents_and_scores( files_cell_array )
 %READ_FILES_CONTENTS_AND_SCORES Summary of this function goes here
 %   Detailed explanation goes here
-review_array ={};
-review_score ={};
+review_array = cell(size(files_cell_array));
+review_score = cell(size(files_cell_array));
 
 % go over all the review files,
 % extract each review string and store it
+fprintf('\nProcessing reviews. Done processing (out of %d):\n', size(files_cell_array,1));
 for i = 1:size(files_cell_array,1)
-    disp(sprintf('Processing review %d out of %d', i, size(files_cell_array,1)));
+    if (mod(i,25) == 0) || (i == size(files_cell_array,1))
+        fprintf('%d ',i);
+    end
+    if (i == size(files_cell_array,1)) || (mod(i,1000) == 0 && i > 0)
+        fprintf('\n');
+    end
     myfile = files_cell_array{i};
     
-    score=strsplit(myfile,'\');%score of review
-    score=score(size(score,2));
-    score=score{1};
-    score=score(size(score,2)-4);
-    score=str2num(score);
+    split_path=strsplit(myfile,'\');%score of review
+    filename_cell=split_path(end);
+    filename_str=filename_cell{1};
+    filename_parts=strsplit(filename_str,{'_','.'});
+    score=filename_parts(2);
+    score=str2double(score);
     if (score==4 || score==7)
         score=1;
     elseif (score==3 || score==8)
         score=2;
     elseif (score==2 || score==9)
         score=3;
-    else
+    elseif (score==1 || score==10)
         score=4;
+    else
+        fprintf(2,'Warning: score %d not in 7-10 or 1-4, for file %s\n', score, myfile);
     end
      
     fid = fopen( myfile);
@@ -32,11 +41,9 @@ for i = 1:size(files_cell_array,1)
         mystr = strcat(mystr, s{1,1}{mycellindex});
     end
     fclose(fid);
-    review_array{end+1} = mystr;
-    review_score{end+1}=score;
+    review_array{i} = mystr;
+    review_score{i}=score;
 end
-review_array = review_array';
-review_score = review_score';
 
 end
 
