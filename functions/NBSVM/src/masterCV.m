311ccb2330532537f60956caa4d7cea079ba9466
ofri <ofrigalperin@gmail.com>
2016-02-21 22:16:48 +0200

    uncomment original code

--- functions/NBSVM/src/masterCV.m
+++ functions/NBSVM/src/masterCV.m
@@ -12,40 +12,39 @@ if ~isfield(params, 'doCV')
  params.doCV = 1;
 end
 
-% % % trainfuncp = @(allSNumBi, labels, params) trainMNB(allSNumBi, labels, params);
-% % % testfuncp = @(model, allSNumBi, labels, params) testMNB(model, allSNumBi, labels, params);
-% % % 
-% % % if params.doCV
-% % %     CV;
-% % % else
-% % %     trainTest;
-% % % end
-% % % 
-% % % writeResults(allcounts, params, [dataset '-SVM-' gram]);
-% % % allresults([dataset, '-MNB-', gram]) = mean(allcounts);
-% % % % trainfuncp = @(allSNumBi, labels, params) trainBNB(allSNumBi, labels, params);
-% % % % testfuncp = @(model, allSNumBi, labels, params) testBNB(model, allSNumBi, labels, params);
-% % % % 
-% % % % if params.doCV
-% % % %     CV;
-% % % % else
-% % % %     trainTest;
-% % % % end
-% % % % 
-% % % % writeResults(allcounts, params, [dataset '-BNB-' gram]);
-% % % % allresults([dataset, '-BNB-', gram]) = mean(allcounts);
-% % % trainfuncp = @(allSNumBi, labels, params) trainbisvm(allSNumBi, labels, params);
-% % % testfuncp = @(model, allSNumBi, labels, params) testbisvm(model, allSNumBi, labels, params);
-% % % 
-% % % if params.doCV
-% % %     CV;
-% % % else
-% % %     trainTest;
-% % % end
-% % % 
-% % % writeResults(allcounts, params, [dataset '-SVM-' gram]);
-% % % allresults([dataset, '-SVM-', gram]) = mean(allcounts);
+trainfuncp = @(allSNumBi, labels, params) trainMNB(allSNumBi, labels, params);
+testfuncp = @(model, allSNumBi, labels, params) testMNB(model, allSNumBi, labels, params);
 
+if params.doCV
+    CV;
+else
+    trainTest;
+end
+
+writeResults(allcounts, params, [dataset '-SVM-' gram]);
+allresults([dataset, '-MNB-', gram]) = mean(allcounts);
+% trainfuncp = @(allSNumBi, labels, params) trainBNB(allSNumBi, labels, params);
+% testfuncp = @(model, allSNumBi, labels, params) testBNB(model, allSNumBi, labels, params);
+% 
+% if params.doCV
+%     CV;
+% else
+%     trainTest;
+% end
+% 
+% writeResults(allcounts, params, [dataset '-BNB-' gram]);
+% allresults([dataset, '-BNB-', gram]) = mean(allcounts);
+trainfuncp = @(allSNumBi, labels, params) trainbisvm(allSNumBi, labels, params);
+testfuncp = @(model, allSNumBi, labels, params) testbisvm(model, allSNumBi, labels, params);
+
+if params.doCV
+    CV;
+else
+    trainTest;
+end
+
+writeResults(allcounts, params, [dataset '-SVM-' gram]);
+allresults([dataset, '-SVM-', gram]) = mean(allcounts);
 
 trainfuncp = @(allSNumBi, labels, params) trainMNBSVM(allSNumBi, labels, params);
 testfuncp = @(model, allSNumBi, labels, params) testMNBSVM(model, allSNumBi, labels, params);
