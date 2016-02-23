31662f8715903ecd80e75cebf956bf214e3c4073
ofri <ofrigalperin@gmail.com>
2016-02-23 21:30:46 +0200

    updates in classify_nbsvm_script, add a script that loads bow

--- functions/classify_nbsvm_load.m
+++ functions/classify_nbsvm_load.m
@@ -10,43 +10,26 @@ labels = [ones(size(posfiles,1),1); zeros(size(negfiles,1),1)];
 allfiles = [posfiles; negfiles];
 [review_array, review_score] = read_files_contents_and_scores( allfiles );
 clear allfiles negfiles posfiles
-%% Convert the textual review into a bag of words
-% We select a specific 'bag of words' for the features.
-% These features will be the coordinates in the vector representation of
-% the review
-[unigram_corpus,unigram_non_corpus,bigram_corpus,bigram_non_corpus,...
-    map_sizes] = build_bag_of_words(review_array, 1, 1);
+%% Load existing full bad of words
 bow_name = ['trained_models\',train_set_name,'_bow_full_stem_stop'];
-mkdir(['trained_models\',train_set_name,'_bow_full_stem_stop']);
-save_map([bow_name,'\unigram_corpus.mat'],unigram_corpus);
-save_map([bow_name,'\unigram_non_corpus.mat'],unigram_non_corpus);
-save_map([bow_name,'\bigram_corpus.mat'],bigram_corpus);
-save_map([bow_name,'\bigram_non_corpus.mat'],bigram_non_corpus);
+unigram_corpus = load_map([bow_name,'\unigram_corpus.mat']);
+unigram_non_corpus = load_map([bow_name,'\unigram_non_corpus.mat']);
+bigram_corpus = load_map([bow_name,'\bigram_corpus.mat']);
+bigram_non_corpus = load_map([bow_name,'\bigram_non_corpus.mat']);
+map_sizes = [length(keys(unigram_corpus)), ...
+             length(keys(unigram_non_corpus)), ...
+             length(keys(bigram_corpus)), ...
+             length(keys(bigram_non_corpus))];
 %% filter bag of words using thresholds for each kind of term
-
-% % Original thresholds
-% params.unigram_corpus_thresh = 0.0001; % 0.0001
-% params.unigram_not_corpus_thresh = 0.05; % 0.05
-% params.bigram_corpus_thresh = 0.001; % 0.0001
-% params.bigram_not_corpus_thresh = 0.05; % 0.05
-
-% % 26293
-% params.unigram_corpus_thresh = 0;
-% params.unigram_not_corpus_thresh = 0.00001;
-% params.bigram_corpus_thresh = 0.00001;
-% params.bigram_not_corpus_thresh = 0.00005;
-
-% % about 322000
-params.unigram_corpus_thresh = 0;
-params.unigram_not_corpus_thresh = 1;
-params.bigram_corpus_thresh = 0;
-params.bigram_not_corpus_thresh = 1;
+params.unigram_corpus_thresh = 0;     % 3719
+params.unigram_not_corpus_thresh = 1; % 0
+params.bigram_corpus_thresh = 0; % 16745
+params.bigram_not_corpus_thresh = 1;  % 0
 filtered_bag_of_words = filter_bag_of_words(unigram_corpus,...
     unigram_non_corpus,bigram_corpus,bigram_non_corpus,map_sizes,params);
-% clear bigram_corpus unigram_corpus unigram_non_corpus bigram_non_corpus
+%clear bigram_corpus unigram_corpus unigram_non_corpus bigram_non_corpus
 %% Convert review to features
 featureVector = featurize_bigram(filtered_bag_of_words,review_array, 1, 1);
-% featureVector = weight_features_by_score(featureVector , review_score);
 filename = sprintf('trained_models\\%s_feature_matrix_V%d.mat',...
     train_set_name,size(featureVector,2));
 save_features(filename,featureVector);
@@ -54,12 +37,11 @@ filename = sprintf('trained_models\\%s_filtered_bow_V%d.mat',...
     train_set_name,size(featureVector,2));
 save(filename,'filtered_bag_of_words');
 %% Train NBSVM model
+% FROM MASTER
 params.C = 1;
 params.samplenum = 1;
 params.samplerate = 1;
 params.Cbisvm = 0.1;
-params.a = 1; % params.a is the Laplacian smoothing parameter
-params.beta = 0.25; % beta is the interpolation parameter
 
 % this is the exponent used to discount raw counts
 % set to 1 to use raw counts f, 
@@ -67,6 +49,13 @@ params.beta = 0.25; % beta is the interpolation parameter
 params.testp = 0;
 params.trainp = 0;
 
+% params.a is the Laplacian smoothing parameter
+params.a = 1;
+% beta is the interpolation parameter
+params.beta = 0.25;
+
+% FROM MASTERCV (instead of loading saved data, performs format 
+% transformation functions)
 % cross validation parameters (TODO: NOT NEEDED???????????)
 params.CVNUM = 1;
 params.doCV = 0;
@@ -77,7 +66,7 @@ params.doCV = 0;
 % labels = labels(p);
 
 % Our code
-labels_nbsvm = logical(labels'); % labels2nbsvm_format(1-labels);
+labels_nbsvm = logical(labels');%labels2nbsvm_format(1-labels);
 allSNumBi = features2nbsvm_format(featureVector);
 wordsbi = filtered_bag_of_words;
 
@@ -95,7 +84,7 @@ end
 
 trainfuncp = @(allSNumBi, labels, params) trainMNBSVM(allSNumBi, labels, params);
 testfuncp = @(model, allSNumBi, labels, params) testMNBSVM(model, allSNumBi, labels, params);
-
+% FROM TRAINTEST
 fprintf('Train using dataset l=%d, dictSize=%d, CVNUM=%d\n', ...
     length(allSNumBi), length(filtered_bag_of_words), params.CVNUM)
 
@@ -103,11 +92,15 @@ fprintf('Train using dataset l=%d, dictSize=%d, CVNUM=%d\n', ...
 %rand('state', 0);
 
 model = trainfuncp(allSNumBi, labels_nbsvm, params);
+
+save(sprintf('trained_models\nbsvm_models\nbsvm_V%d.mat',params.dictsize),'model');
 %% Load negative and positive examples from test set
-test_name = 'test_stan';
-negfiles_test = getAllFiles(['test_sets\',test_name,'\neg\']);
-posfiles_test = getAllFiles(['test_sets\',test_name,'\pos\']);
+train_set_name = 'test_stan';
+negfiles_test = getAllFiles(['test_sets\',train_set_name,'\neg\']);
+posfiles_test = getAllFiles(['test_sets\',train_set_name,'\pos\']);
+%labels_test = [zeros(size(negfiles_test,1),1); ones(size(posfiles_test,1),1)];
 labels_test = [ones(size(posfiles_test,1),1);zeros(size(negfiles_test,1),1)];
+%allfiles_test = [negfiles_test;posfiles_test];
 allfiles_test = [posfiles_test;negfiles_test];
 [review_array_test, review_score_test] = read_files_contents_and_scores( allfiles_test );
 %% Convert the textual review into a feature vector (and locally save the feature vectors) 
